4e6470d150735bf11093c1d14474f31056b2a8d1
andywschoi0330 <andywschoi0330@gmail.com>
2026-07-01 13:21:04 +0900

    Delete api directory

--- api/admin/logout.js
+++
@@ -1,6 +0,0 @@
-const {clearCookie, requireMethod} = require('../_lib/security');
-module.exports = async function handler(req, res) {
-  if (!requireMethod(req, res, 'POST')) return;
-  clearCookie(res, 'atm_admin');
-  res.status(200).json({ok: true});
-};
--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const {clearCookie, requireMethod} = require('./_lib/security');
-module.exports = async function handler(req, res) {
-  if (!requireMethod(req, res, 'POST')) return;
-  clearCookie(res, 'atm_session');
-  res.status(200).json({ok: true});
-};
